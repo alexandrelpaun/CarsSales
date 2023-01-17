b6e1d4369065804d2266489b308b9bf9aa95dad8
Alex Bordei <alex.bordei@whiz.ro>
2023-01-17 18:24:18 +0200

    adding comments

--- lib/models/announcements_cars.dart
+++ lib/models/announcements_cars.dart
@@ -6,19 +6,23 @@ import 'package:http/http.dart' as http;
 import 'dart:async';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+// 1. Am convertit Stateless in StateFull ca sa pot schimba starea widgetului cu setState
 class AnnouncementsCars extends StatefulWidget {
-  AnnouncementsCars({super.key});
+  const AnnouncementsCars({super.key});
 
   @override
   State<AnnouncementsCars> createState() => _AnnouncementsCarsState();
 }
 
 class _AnnouncementsCarsState extends State<AnnouncementsCars> {
+  // 2. isLoading are ca scop sa marcheze ca aplicatia este in loading la primul request (atunci cand se incarca)
   bool isLoading = false;
+  // 3. O lista de anunturi pentru a fi trimisa catre clasa Card
   List<AnnouncementModel> announcements = [];
 
   @override
   void initState() {
+    // 4. Init() face incarcarea initiala a datelor
     init();
     super.initState();
   }
@@ -26,15 +30,20 @@ class _AnnouncementsCarsState extends State<AnnouncementsCars> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      // 8. Daca isLoading este true -> afiseaza CircularProgressIndicator
       body: isLoading
           ? const Center(
               child: CircularProgressIndicator(),
             )
+          // 9. Daca isLoading este false, atunci afiseaza Cars(anunturi)
+          // 10. RefreshIndicator contine WidgetulCard
           : RefreshIndicator(
+              // 11. Cand lista a fost trasa in jos, faceam un call nou API si incarcam raspunsul intr-o variabila locala apiResult
+              // 12. in tot timpul apelului API, loader-ul de la Refreshindicator va fi afisat
               onRefresh: () async {
                 List<AnnouncementModel> apiResult =
                     await fetchAnnouncementModel();
-
+                // 13. Dupa finalizarea call API, incarcam noul raspuns API in lista proprietatea a clasei noastre
                 setState(() {
                   announcements = apiResult;
                 });
@@ -47,12 +56,15 @@ class _AnnouncementsCarsState extends State<AnnouncementsCars> {
   }
 
   void init() async {
+    // 5. Marcam isLoading = true, si updatam starea aplicatiei, ca sa fie afisat CircularProgressIndicator()
     setState(() {
       isLoading = true;
     });
 
+    // 6. Intr-o variabila numita apiResult stocam raspunsul din API
     List<AnnouncementModel> apiResult = await fetchAnnouncementModel();
 
+    // 7. Dupa ce s-a terminat request-ul catre api marcam isLoading ca fiind false si incarcam datele in proprietatea clasei 'announcement'
     setState(() {
       isLoading = false;
       announcements = apiResult;
